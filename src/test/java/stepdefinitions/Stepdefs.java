029e93baf6640c14b1627aed561a95e2a236ee4c
sangeethkumar <sangeethkumarasp@gmail.com>
2021-07-22 15:18:20 +0530

    step definition file added

--- src/test/java/stepdefinitions/Stepdefs.java
+++ src/test/java/stepdefinitions/Stepdefs.java
@@ -3,26 +3,51 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import resources.Resouces;
+import resources.TestDataBuild;
 import resources.Utility;
+import static io.restassured.RestAssured.given;
+
+import java.io.FileNotFoundException;
 
 public class Stepdefs extends Utility {
 	
-	
+	RequestSpecification res;
+	TestDataBuild data =new TestDataBuild();
+	Response response;
 	
 	
 	
 	
 	@Given("user with addplace payload with {string} {string} {string}")
-	public void user_with_addplace_payload_with(String string, String string2, String string3) {
+	public void user_with_addplace_payload_with(String name, String language, String address) throws FileNotFoundException {
 		
+		res=given().spec(requestspecification()).body(data.addPlacePayLoad(name, language, address));
 		
-		
+	
 	  
 	}
 
 	@When("user calls {string} with {string} http request")
-	public void user_calls_with_http_request(String string, String string2) {
+	public void user_calls_with_http_request(String resource, String crudmethod) {
+		
+		Resouces apiresources=	Resouces.valueOf(resource);
+		
+	   System.out.println(apiresources.getresouces());
 	   
+	   if(crudmethod.equalsIgnoreCase("post"))
+	   {
+		 response=  res.when().post(apiresources.getresouces());
+	   }else if(crudmethod.equalsIgnoreCase("GET"))
+	   {
+		   response =  res.when().get(apiresources.getresouces());
+	   }
+	   
+	   
+	  
 	}
 
 	@Then("api call got sucess with status code {int}")
